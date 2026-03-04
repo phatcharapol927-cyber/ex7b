cd9097adf8112feede0023f66fe826ac8b850b92
Phatcharapol <phatcharapol927@gmail.com>
2026-03-04 15:22:45 +0700

    Bug fix#3 -  revise candidate no. validation

--- validate.js
+++ validate.js
@@ -10,12 +10,19 @@ function checkSID() {
 }
 
 function checkCandiNo() {
-  let candi = (document.getElementById("candi").value).trim();
-  if (isNaN(candi) || candi < 1) {
-    return false;
-  } else {
-    return true;
-  }
+    let candi = document.getElementById("candi").value.trim();
+
+    if (!/^[0-9]+$/.test(candi)) {
+        return false;
+    }
+
+    let number = parseInt(candi);
+
+    if (number >= 1 && number <= 10) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 function validateForm(){
